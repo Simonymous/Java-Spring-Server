ecf1fb7663db6cf7090db4ba9f48c2f1014362ec
Simon <sito.prvt@t-online.de>
2020-09-03 15:38:36 +0200

    added first test

--- src/test/java/htw/teamsimon/serverapplication/ServerapplicationApplicationTests.java
+++ src/test/java/htw/teamsimon/serverapplication/ServerapplicationApplicationTests.java
@@ -1,13 +1,29 @@
 package htw.teamsimon.serverapplication;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class ServerapplicationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	final String baseUrl = "http://localhost:8080/";
 
+	@Test
+	void registrationTest() throws URISyntaxException {
+		RestTemplate restTemplate = new RestTemplate();
+
+		String url = baseUrl + "register?name=test&password=test";
+		URI uri = new URI(url);
+
+		ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+		Assert.assertEquals(200, result.getStatusCodeValue());
+		Assert.assertEquals(true, result.getBody().contains("Registration of user test successfull"));
+	}
 }
